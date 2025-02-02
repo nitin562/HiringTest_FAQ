a504d792878fcb4a59cbea29b49b352fcc360741
nitindb <nitindbas8800@gmail.com>
2025-02-02 12:07:26 +0530

    Production Url placement

--- frontend/links.js
+++ frontend/links.js
@@ -1,5 +1,6 @@
 export const links = {
-  base: "http://localhost:8000",
+  // base: "http://localhost:8000",
+  base:"https://hiringtest-faq-backend.onrender.com",
   getFaqs: "/api/faqs",
   translateTo: "/api/faq",
   addFaq: "/api/faq",
