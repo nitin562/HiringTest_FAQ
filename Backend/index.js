ad3ea3702553a4aa3572247c5126921fe5eb013c
nitindb <nitindbas8800@gmail.com>
2025-02-02 13:32:03 +0530

    fix: env variables for url

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const app=express()
 const port=process.env.PORT||8000
 app.use(express.json())
 app.use(cors({
-    origin:"*"
+    origin:process.env.frontend
 }))
 const connectDb = require("./ConnectToDb")
 
--- frontend/links.js
+++ frontend/links.js
@@ -1,6 +1,6 @@
 export const links = {
-  base: "http://localhost:8000",
-  // base:"https://hiringtest-faq-backend.onrender.com",
+  // base: "http://localhost:8000",
+  base:"https://hiringtest-faq-backend.onrender.com",
   getFaqs: "/api/faqs",
   translateTo: "/api/faq",
   addFaq: "/api/faq",
